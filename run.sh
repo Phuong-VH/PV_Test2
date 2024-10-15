24a8d122b26752289ac41d3d959a2699cdf8b108
PhuongVH <vanhoangphuong1610@gmail.com>
2024-10-15 12:20:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ pip3 install catboost
 # pip3 install Pillow image mealpy;
 pip3 install --upgrade tensorflow
 pip3 install --upgrade numpy pandas sklearn pillow;
-python3 ./PV_Test/CNN_PSO.py;
+python3 ./PV_Test2/CNN_PSO.py;
